bd852ea2f58babdeefaece9197527a472344676d
karthik <karthikkannan@sensehq.com>
2022-09-27 23:25:28 +0530

    [dev-utils #1] Support to remove local branches without remote tracking branch.

---
+++ src/cli/tasks.py
@@ -0,0 +1,32 @@
+from invoke import Program
+from invoke import task, Collection
+
+
+@task
+def git_clean(c):
+    c.run("git fetch --prune")
+
+    branches = []
+    try:
+        gone_branches = c.run("git branch -vv  | grep  gone", hide='out')
+        gone_branches = [b.strip().split(' ')[0] for b in gone_branches.stdout.strip().split('\n')]
+        branches.extend(gone_branches)
+    except:
+        print("No Gone branches")
+
+    try:
+        branches_without_origin = c.run("git branch -vv  | grep  -v origin", hide='out')
+        branches_without_origin = [b.strip().split(' ')[0] for b in branches_without_origin.stdout.strip().split('\n')]
+        branches.extend(branches_without_origin)
+    except:
+        print("No branches without origin")
+
+    for branch in branches:
+        print(f"Deleting branch: {branch}")
+        c.run(f"git branch -D {branch}")
+
+
+if __name__ == '__main__':
+    current_module = __import__(__name__)
+    program = Program(namespace=Collection.from_module(current_module), version='0.1.0')
+    program.run('git-clean git-clean')
